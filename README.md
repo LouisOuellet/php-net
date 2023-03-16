38ba8464f24210367159da9e5633a7e485e1a76e
Louis Ouellet <louis_ouellet@hotmail.com>
2023-03-16 12:48:11 -0400

    2023-03-16 12:48 - v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# php-net
-Network Tools Library for PHP
+![GitHub repo logo](/dist/img/logo.png)
+
+# phpNet
+![License](https://img.shields.io/github/license/LouisOuellet/php-net?style=for-the-badge)
+![GitHub repo size](https://img.shields.io/github/repo-size/LouisOuellet/php-net?style=for-the-badge&logo=github)
+![GitHub top language](https://img.shields.io/github/languages/top/LouisOuellet/php-net?style=for-the-badge)
+![Version](https://img.shields.io/github/v/release/LouisOuellet/php-net?label=Version&style=for-the-badge)
+
+## Description
+The phpNet library provides functionality to perform network-related tasks such as scanning a port, sending a ping, and performing a DNS lookup.
+
+## Features
+ - Allows for scanning a specified port on a specified IP address or domain name
+ - Provides a method for sending a ping request to a specified IP address or domain name and returns the latency on success or false on failure
+ - Allows for performing DNS lookups for a specified hostname and record type (A, AAAA, CNAME, MX, NS, PTR, SOA, TXT)
+ - Uses the phpLogger library to log information, errors, and successes
+ - Can be configured with options like logging level to control the amount of output produced by the library
+
+## Why you might need it?
+You might need it if you're developing a web application or network monitoring tool that requires such functionality. For example, you may need to check if a particular port is open on a server, or monitor the availability of a website by sending a ping request. Additionally, the phpNet library can be used to automate tasks like network reconnaissance and penetration testing.
+
+## Can I use this?
+Sure!
+
+## License
+This software is distributed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html) license. Please read [LICENSE](LICENSE) for information on the software availability and distribution.
+
+## Requirements
+PHP >= 7.0
+
+## Security
+Please disclose any vulnerabilities found responsibly – report security issues to the maintainers privately.
+
+## Installation
+Using Composer:
+```sh
+composer require laswitchtech/php-net
+```
+
+## How do I use it?
+In this documentations, we will use a table called users for our examples.
+
+### Initialize
+```php
+
+// Import phpNet class into the global namespace
+// These must be at the top of your script, not inside a function
+use LaswitchTech\phpNet\phpNet;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Initialize phpNet
+$phpNet = new phpNet();
+```
+### Methods
+#### config($option, $value)
+Configures the library with the specified `option` and `value`. The only supported option is `level` which specifies the logging level.
+```php
+// Configure phpNet Log Level to Debug
+$phpNet->config("level",5);
+```
+
+#### scan($ip, $port, $timeout)
+Scans a network port on the specified IP address. Returns `true` if the port is open, and `false` otherwise.
+```php
+// Scan a Port
+$phpNet->scan("google.com","HTTPS");
+$phpNet->scan("google.com",443);
+```
+
+#### ping($ip)
+Sends a ping to the specified IP address and returns the latency on success or `false` on failure.
+```php
+// Ping an IP
+$phpNet->ping("127.0.0.1");
+$phpNet->ping("::1");
+$phpNet->ping("google.com");
+```
+
+#### lookup($hostname, $type)
+Performs a DNS lookup for the specified hostname and returns an array of IP addresses on success, or `false` on failure.
+```php
+// Lookup a Domain
+$phpNet->lookup("google.com");
+$phpNet->lookup("google.gov.us");
+```
---
+++ src/phpNet.php
@@ -0,0 +1,371 @@
+<?php
+
+//Declaring namespace
+namespace LaswitchTech\phpNet;
+
+//Import phpLogger class into the global namespace
+use LaswitchTech\phpLogger\phpLogger;
+
+//Import Exception class into the global namespace
+use \Exception;
+
+class phpNet {
+
+	// List of ports
+	const ListTCP = [
+    "FTP DATA" => 20,
+    "FTP CONTROL" => 21,
+    "FTP" => 21,
+    "SSH" => 22,
+    "TELNET" => 23,
+    "SMTP" => 25,
+    "DNS" => 53,
+    "HTTP" => 80,
+    "POP3" => 110,
+    "NNTP" => 119,
+    "NTP" => 123,
+    "IMAP" => 143,
+    "SNMP" => 161,
+    "IRC" => 194,
+    "HTTPS" => 443,
+    "SMTPS" => 465,
+    "IMAPS" => 993,
+    "POP3S" => 995,
+    "VPN" => 1194,
+    "OPENVPN" => 1194,
+    "SQL" => 3306,
+    "MYSQL" => 3306,
+    "MARIADB" => 3306,
+    "PROXMOX" => 8006,
+    "ISPCONFIG" => 8080,
+  ];
+	const ListUDP = [
+    "DNS" => 53,
+    "DHCP CLIENT" => 67,
+    "DHCP SERVER" => 68,
+    "DHCP" => 68,
+    "TFTP" => 69,
+    "NTP" => 123,
+    "NETBIOS" => 137,
+    "NETBIOS NAME SERVICE" => 137,
+    "NETBIOS DATAGRAM SERVICE" => 138,
+    "SNMP" => 161,
+    "IKEV1" => 500,
+    "ISAKMP" => 500,
+    "IKEV1 / ISAKMP" => 500,
+    "SYSLOG" => 514,
+    "RIP" => 520,
+    "VPN" => 1194,
+    "OPENVPN" => 1194,
+    "UPNP" => 1900,
+    "MDNS" => 5353,
+    "BONJOUR" => 5353,
+    "MDNS / BONJOUR" => 5353,
+    "NETBUS" => 12345,
+    "STEAM GAME SERVER" => 27015,
+  ];
+	const Ports = [
+    "FTP DATA" => 20,
+    "FTP CONTROL" => 21,
+    "FTP" => 21,
+    "SSH" => 22,
+    "TELNET" => 23,
+    "SMTP" => 25,
+    "DNS" => 53,
+    "HTTP" => 80,
+    "POP3" => 110,
+    "NNTP" => 119,
+    "NTP" => 123,
+    "NETBIOS" => 137,
+    "NETBIOS NAME SERVICE" => 137,
+    "NETBIOS DATAGRAM SERVICE" => 138,
+    "IMAP" => 143,
+    "SNMP" => 161,
+    "IRC" => 194,
+    "HTTPS" => 443,
+    "SMTPS" => 465,
+    "ISAKMP" => 500,
+    "IKEV1" => 500,
+    "IKEV1 / ISAKMP" => 500,
+    "SYSLOG" => 514,
+    "RIP" => 520,
+    "DHCP CLIENT" => 67,
+    "TFTP" => 69,
+    "UPNP" => 1900,
+    "MDNS" => 5353,
+    "BONJOUR" => 5353,
+    "MDNS / BONJOUR" => 5353,
+    "POP3S" => 995,
+    "IMAPS" => 993,
+    "VPN" => 1194,
+    "OPENVPN" => 1194,
+    "SQL" => 3306,
+    "MYSQL" => 3306,
+    "MARIADB" => 3306,
+    "DHCP SERVER" => 68,
+    "DHCP" => 68,
+    "PROXMOX" => 8006,
+    "ISPCONFIG" => 8080,
+    "NETBUS" => 12345,
+    "STEAM GAME SERVER" => 27015,
+  ];
+	const Timeout = 5;
+
+	// Logger
+	private $Logger;
+	private $Level = 1;
+
+  /**
+   * Create a new phpNet instance.
+   *
+   * @param  string|null  $host
+   * @param  string|null  $username
+   * @param  string|null  $password
+   * @param  string|null  $database
+   * @return void
+   * @throws Exception
+   */
+  public function __construct() {
+
+    // Initiate phpLogger
+    $this->Logger = new phpLogger(['database' => 'log/database.log']);
+
+    // Configure phpLogger
+    $this->Logger->config('ip',true);
+    $this->Logger->config('rotation',false);
+    $this->Logger->config('level',$this->Level);
+  }
+
+  /**
+   * Configure Library.
+   *
+   * @param  string  $option
+   * @param  bool|int  $value
+   * @return void
+   * @throws Exception
+   */
+  public function config($option, $value){
+		try {
+			if(is_string($option)){
+	      switch($option){
+	        case"level":
+	          if(is_int($value)){
+
+							// Logging Level
+	            $this->Level = $value;
+
+							// Configure phpLogger
+					    $this->Logger->config('level',$this->Level);
+	          } else{
+	            throw new Exception("2nd argument must be an integer.");
+	          }
+	          break;
+	        default:
+	          throw new Exception("unable to configure $option.");
+	          break;
+	      }
+	    } else{
+	      throw new Exception("1st argument must be as string.");
+	    }
+		} catch (Exception $e) {
+			$this->Logger->error('Error: '.$e->getMessage());
+		}
+  }
+
+  /**
+   * Scan a port.
+   *
+   * @param  string  $ip
+   * @param  string|int  $port
+   * @param  int  $timeout
+   * @return bool
+   * @throws Exception
+   */
+  public function scan($ip, $port, $timeout = self::Timeout){
+		try{
+
+			// Debug Information
+			$this->Logger->debug("Scanning Port");
+			$this->Logger->debug("IP: {$ip}");
+			$this->Logger->debug("Port: {$port}");
+			$this->Logger->debug("Timeout: {$timeout}");
+
+			// Validate IP Address
+			if(!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE) && !filter_var($ip, FILTER_VALIDATE_DOMAIN)){
+				throw new Exception("You need to supply a valid IPv4 address, IPv6 address or Fully Qualified Domain Name (FQDN).");
+			}
+
+			// Validate Port Number
+			if(!is_string($port) && !is_int($port)){
+				throw new Exception("Invalid network port.");
+			}
+			if(is_string($port)){
+
+				// Sanitize Port Name
+				$port = strtoupper($port);
+
+				// Check if known
+				if(!isset(self::Ports[$port])){
+					throw new Exception("Unknown network port.");
+				}
+
+				// Convert Port Name to Number
+				$port = self::Ports[$port];
+			}
+
+			// Validate Timeout
+			if(!is_int($timeout)){
+				throw new Exception("Invalid timeout.");
+			}
+
+			// Scan Port
+			$socket = @fsockopen($ip, $port, $errno, $errstr, $timeout);
+
+			// Handle Result
+			if($socket){
+
+				// Close Socket
+				fclose($socket);
+
+				// Logger
+				$this->Logger->success("Port {$port} on {$ip} is open.");
+
+				// Return True
+				return true;
+			} else {
+
+				// Debug Information
+				$this->Logger->debug("Error Number: {$errno}");
+				$this->Logger->debug("Error String: {$errstr}");
+
+				// Logger
+				$this->Logger->error("Port {$port} on {$ip} is closed or blocked.");
+
+				// Return False
+				return false;
+			}
+		} catch (Exception $e) {
+			$this->Logger->error('Error: '.$e->getMessage());
+		}
+  }
+
+  /**
+   * Send a ping.
+   *
+   * @param  string  $ip
+   * @param  int  $timeout
+   * @return string|bool Return latency on success and false on failure
+   * @throws Exception
+   */
+  public function ping($ip){
+		try{
+
+			// Debug Information
+			$this->Logger->debug("Sending Ping");
+			$this->Logger->debug("IP: {$ip}");
+
+			// Validate IP Address
+			if(!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE) && !filter_var($ip, FILTER_VALIDATE_DOMAIN)){
+				throw new Exception("You need to supply a valid IPv4 address, IPv6 address or Fully Qualified Domain Name (FQDN).");
+			}
+
+			// Construct the ping request
+			$ping = sprintf('ping -c 1 %s', escapeshellarg($ip));
+			$output = '';
+
+			// Debug Information
+			$this->Logger->debug("Request: {$ping}");
+
+			// Send the ping request
+			exec($ping, $output, $status);
+
+			// Check if the ping was successful
+			if ($status === 0) {
+
+				// Sanitize Resonse
+				if(!isset($output[1])){
+					throw new Exception("Unable to retrieve the command's response.");
+				}
+				$output = $output[1];
+
+				// Retrieve Latency
+				$output = preg_match('/time=([\d.]+)/', $output, $matches);
+				if($output){
+					$output = $matches[1];
+				} else {
+					throw new Exception("Unable to retrieve latency.");
+				}
+
+				// Logger
+				$this->Logger->info("Ping latency is {$output}ms.");
+				$this->Logger->success("Ping to {$ip} was successful.");
+
+				// Return Latency
+				return $output;
+			} else {
+
+				// Logger
+				$this->Logger->error("Ping to {$ip} failed.");
+
+				// Return False
+				return false;
+			}
+		} catch (Exception $e) {
+			$this->Logger->error('Error: '.$e->getMessage());
+		}
+  }
+
+	/**
+	 * Perform a DNS lookup.
+	 *
+	 * @param  string  $hostname
+	 * @param  string  $type
+	 * @return mixed  Returns an array of IP addresses on success, or false on failure
+	 * @throws Exception
+	 */
+	public function lookup($hostname, $type = 'A') {
+    try {
+      // Debug Information
+      $this->Logger->debug("Performing DNS Lookup");
+      $this->Logger->debug("Hostname: {$hostname}");
+      $this->Logger->debug("Type: {$type}");
+
+      // Validate hostname
+      if (!filter_var($hostname, FILTER_VALIDATE_DOMAIN)) {
+        throw new Exception("Invalid hostname.");
+      }
+
+      // Validate DNS record type
+      if (!in_array($type, ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'PTR', 'SOA', 'TXT'])) {
+        throw new Exception("Invalid DNS record type.");
+      }
+
+      // Perform the DNS lookup
+      $result = dns_get_record($hostname, constant('DNS_' . $type));
+      if ($result === false) {
+        throw new Exception("DNS lookup failed.");
+      }
+
+      // Extract IP addresses from the results
+      $ips = array_filter(array_map(function($r) {
+        if ($r['type'] === 'A' || $r['type'] === 'AAAA') {
+          return $r['ip'];
+        }
+        return null;
+      }, $result));
+
+      // Logger
+      if (count($ips) > 0) {
+        $this->Logger->success("DNS lookup for {$hostname} succeeded. IP addresses: " . implode(', ', $ips));
+      } else {
+        $this->Logger->error("DNS lookup for {$hostname} succeeded, but no IP addresses were found.");
+      }
+
+      // Return IP addresses or false
+      return count($ips) > 0 ? $ips : false;
+    } catch (Exception $e) {
+      $this->Logger->error('Error: ' . $e->getMessage());
+      return false;
+    }
+	}
+}

489901d0f56a0681997d55e7b11400917d933992
Louis Ouellet <louis_ouellet@hotmail.com>
2023-04-03 12:52:44 -0400

    2023-04-03 12:52 - v1.1.5

--- src/phpNet.php
+++ src/phpNet.php
@@ -155,18 +155,6 @@ class phpNet {
 		try {
 			if(is_string($option)){
 	      switch($option){
-	        case"level":
-	          if(is_int($value)){
-
-							// Logging Level
-	            $this->Level = $value;
-
-							// Configure phpLogger
-					    $this->Logger->config('level',$this->Level);
-	          } else{
-	            throw new Exception("2nd argument must be an integer.");
-	          }
-	          break;
 	        default:
 	          throw new Exception("unable to configure $option.");
 	          break;
